fd154a86f0c1f63a4a3bf61bdb093890bba3e11d
Karthi <bgkarthikeyan@gmail.com>
2019-03-04 18:19:14 +0100

    asmiqacademyapp adjustments

--- spring-framework-I/3-asmiq-academy-app/src/main/java/ch/asmiq/AsmiqAcademyApp.java
+++ spring-framework-I/3-asmiq-academy-app/src/main/java/ch/asmiq/AsmiqAcademyApp.java
@@ -1,7 +1,9 @@
 package ch.asmiq;
 
+import java.util.Optional;
 import java.util.logging.Logger;
 
+import ch.asmiq.model.Course;
 import ch.asmiq.model.Customer;
 import ch.asmiq.model.Order;
 import ch.asmiq.service.AsmiqAcademyService;
@@ -15,16 +17,40 @@ public class AsmiqAcademyApp {
 	public static void main(String[] args) {
 		CourseService courseService = new CourseService();
 		SmsService smsService = new SmsService();
-		AsmiqAcademyService academyService = new AsmiqAcademyService(courseService, smsService);
-		var optJavaCourse = academyService.getCourses().stream().filter(p -> p.getName().contains("Java")).findAny();
+		AsmiqAcademyService asmiqAcademyService = new AsmiqAcademyService(courseService, smsService);
 
-		if (optJavaCourse.isPresent()) {
-			Order order = new Order();
-			order.setCustomer(new Customer("karthi", "karthistr 6, 8051 Zürich"));
-			order.setCourse(optJavaCourse.get());
-			academyService.placeOrder(order);
-		} else {
-			LOG.info("No order has been placed!!");
-		}
+		var optJavaCourse = findFirstCourseByTopic(asmiqAcademyService, "Java");
+
+		var karthi = new Customer("karthi", "karthistr 6, 8051 Zürich");
+		var michael = new Customer("michael", "karthistr 6, 8051 Zürich");
+
+		placeOrder(asmiqAcademyService, optJavaCourse, karthi);
+		placeOrder(asmiqAcademyService, optJavaCourse, michael);
+
 	}
+
+	private static void placeOrder(AsmiqAcademyService academyService, Optional<Course> optCourse, Customer customer) {
+
+		optCourse.ifPresentOrElse(courseName -> {
+			Order order = createOrder(courseName, customer);
+			academyService.placeOrder(order);
+		}, AsmiqAcademyApp::handleCourseNotPresent);
+
+	}
+
+	private static Order createOrder(Course courseName, Customer customer) {
+		Order order = new Order();
+		order.setCustomer(customer);
+		order.setCourse(courseName);
+		return order;
+	}
+
+	private static Optional<Course> findFirstCourseByTopic(AsmiqAcademyService academyService, String topic) {
+		return academyService.getCourses().stream().filter(p -> p.getName().contains(topic)).findAny();
+	}
+
+	private static void handleCourseNotPresent() {
+		LOG.info("No order has been placed!!");
+	}
+
 }
